664bba27067ef55312c1548e4e4caf3526b220c1
gedrimas <a.a.gedrimas@gmail.com>
2019-07-30 23:29:59 +0300

    working boilerplate

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var User = require('./api/models/userModel'),
+jsonwebtoken = require("jsonwebtoken");
+
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
@@ -13,9 +16,18 @@ mongoose.connect('mongodb://localhost/Tododb');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-/* app.use(function(req, res) {
-    res.status(404).send({url: req.originalUrl + ' not found'})
-}); */
+app.use(function(req, res, next) {
+  if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
+      if(err) req.user = undefined;
+      req.user = decode;
+      next();
+    });
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
 
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
